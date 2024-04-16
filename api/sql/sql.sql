4b69c14e2606df8a477d420e5dbf963b35535642
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2024-04-16 20:38:48 -0300

    feat: modelagem do schema de publicacoes e o type

--- api/sql/sql.sql
+++ api/sql/sql.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE IF NOT EXISTS italobook;
 USE italobook;
 
+DROP TABLE IF EXISTS publicacoes;
+DROP TABLE IF EXISTS seguidores;
 DROP TABLE IF EXISTS usuarios;
 
 CREATE TABLE usuarios (
@@ -23,4 +25,16 @@ CREATE TABLE seguidores(
     ON DELETE CASCADE,
 
     primary key(usuario_id, seguidor_id)
+) ENGINE = INNODB;
+
+CREATE TABLE publicacoes (
+    id int auto_increment primary key,
+    titulo varchar(50) not null,
+    conteudo varchar(300) not null,
+    autor_id int not null,
+    FOREIGN KEY(autor_id) 
+    REFERENCES usuarios(id) ON DELETE CASCADE,
+
+    curtidas int default 0,
+    criadaEm timestamp default current_timestamp
 ) ENGINE = INNODB;
