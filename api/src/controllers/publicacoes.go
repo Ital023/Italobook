6c07631d0f50c54c4da0d1530eec7ee318d0be29
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2024-04-18 17:50:46 -0300

    feat: buscar publicacao do usuario (feed)

--- api/src/controllers/publicacoes.go
+++ api/src/controllers/publicacoes.go
@@ -59,7 +59,29 @@ func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
 }
 
 func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
-	
+	usuarioID, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w,http.StatusInternalServerError,erro)
+		return	
+	}
+	defer db.Close()
+
+	repositorios := repositorios.NovoRepositorioPublicacoes(db)
+	publicacoes, erro := repositorios.Buscar(usuarioID)
+
+	if erro != nil {
+		respostas.Erro(w,http.StatusInternalServerError,erro)
+		return	
+	}
+
+	respostas.JSON(w, http.StatusCreated, publicacoes)
+
 }
 
 func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
