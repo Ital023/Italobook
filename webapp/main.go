e89fd2f4b1c094a9a8392327e4054939aeb7668b
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2024-04-19 20:39:48 -0300

    feat: execute template login

--- webapp/main.go
+++ webapp/main.go
@@ -5,14 +5,15 @@ import (
 	"log"
 	"net/http"
 	"webapp/src/router"
+	"webapp/utils"
 )
 
 func main() {
-	fmt.Println("Rodando web app")
-
+	
+	utils.CarregarTemplates()
 
 	r := router.Gerar()
 
-	
+	fmt.Println("Rodando web app")
 	log.Fatal(http.ListenAndServe(":3000", r))
 }
